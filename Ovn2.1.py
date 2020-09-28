fdd895db06430cc9c5c2401f6499fdbe202e2cf5
Untiltablecup <pelle.eriksson@elev.ga.ntig.se>
2020-09-28 13:49:07 +0200

    Update Ovn2.1.py

--- Ovn2.1.py
+++ Ovn2.1.py
@@ -1,15 +1,12 @@
-import math
+i = float(input("Mätarställning i dag?:")) # Läser in mätarställning idag
+v = float(input("Mätarställning för ett år sedan?:")) # Läser in mätarställning för ett år sedan
+b = float(input("Hur mång liter bensin har förbrukats?:")) # Läster in bensin förbruk på ett år
 
-i = float(input("Mätarställning i dag?:"))
-v = float(input("Mätarställning för ett år sedan?:"))
-b = float(input("Hur mång liter bensin har förbrukats?:"))
+m = i - v # Räknar ut hur många mil som man åkt på ett år
+f = b / m # Räknar ut bensin förbruk per mil
 
-m = i - v
-f = b / m
-
-print ("")
-print ("Mätarställning i dag:", i)
-print ("Mätarställning ett år sedan:", v)
-print ("Antal mil:", m)
-print ("Antal liter bensin:", b)
-print ("Förbrukning per mil:", f)
+print ("Mätarställning i dag:", i) # Mätarställning i dag
+print ("Mätarställning ett år sedan:", v) # Mätarställning ett år sedan
+print ("Antal mil:", m) # Antal mil kört på ett år
+print ("Antal liter bensin:", b) # Bensin förbrukning på ett år
+print ("Förbrukning per mil:", f) # Skriver ut bensin förbrukning per mil
