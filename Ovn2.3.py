6ff97c09b9906cbf4e4e873e28c0a4bdda11beba
Untiltablecup <pelle.eriksson@elev.ga.ntig.se>
2020-09-28 14:30:17 +0200

    Create Ovn2.3.py

---
+++ Ovn2.3.py
@@ -0,0 +1,3 @@
+f = float(input("Hur många farenheit?:")) # Läser in farenheit och ger det till variabeln f
+c=(f-32)*5/9 # Räknar ut hur många celsius det är
+print (c ,"grader celsius") # Skriver ut celsius
