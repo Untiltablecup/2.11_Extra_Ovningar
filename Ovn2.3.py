de9e3f376d4bd1471c2af45c27f8beb78dcc3158
Untiltablecup <pelle.eriksson@elev.ga.ntig.se>
2020-11-30 13:13:31 +0100

    Ovn2.3-kap2

--- Ovn2.3.py
+++ Ovn2.3.py
@@ -1,3 +1,10 @@
 f = float(input("Hur många farenheit?:")) # Läser in farenheit och ger det till variabeln f
 c=(f-32)*5/9 # Räknar ut hur många celsius det är
 print (c ,"grader celsius") # Skriver ut celsius
+
+if c >= 20:
+    print ("Mans not hot")
+elif c < 20:
+    print ("I no go brrrrr")
+else:
+    print ("Temprature go brrrrr")
